af491584140ceaee9c7b6e48ed9e03e80ec1dc47
Tri-Adi-Putra <triadiputra.js.com>
2026-02-28 19:57:29 +0700

    Merubah hello menjadi hello w

--- say-hello.go
+++ say-hello.go
@@ -3,5 +3,5 @@ package go_say_hello
 // Perhatikan huruf besar 'S' pada SayHello
 // Di Go-Lang, huruf besar di awal berarti PUBLIC (bisa diakses dari luar)
 func SayHello() string {
-	return "Hello"
+	return "Hello World"
 }
