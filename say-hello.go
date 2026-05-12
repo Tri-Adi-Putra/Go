d31bdd67d844f6288a2dbdf6aea5c4892aeceeb0
Tri-Adi-Putra <98399147+Tri-Adi-Putra@users.noreply.github.com>
2026-05-12 21:34:33 +0700

    Delete say-hello.go

--- say-hello.go
+++
@@ -1,7 +0,0 @@
-package go_say_hello
-
-// Perhatikan huruf besar 'S' pada SayHello
-// Di Go-Lang, huruf besar di awal berarti PUBLIC (bisa diakses dari luar)
-func SayHello(name string) string {
-	return "Hello " + name
-}
